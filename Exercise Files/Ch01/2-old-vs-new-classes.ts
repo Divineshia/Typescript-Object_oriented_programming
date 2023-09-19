87cbb95851b82c2b765e0ad84e437abd032b41c7
Divineshia <divinejeba.sharon@gmail.com>
2023-09-19 11:57:58 -0500

    completed excercise

--- Exercise Files/Ch01/2-old-vs-new-classes.ts
+++ Exercise Files/Ch01/2-old-vs-new-classes.ts
@@ -4,3 +4,11 @@ export class Message {
     id;
 }
 
+//Older version of writing before 2015 , before ES6
+function Message2(title, message, id){
+    this.title=title;
+    this.message = message;
+    this. id = id;
+}
+ 
+const message5 = new Message2("Hello", "world", "1015");
